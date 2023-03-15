01c7f11aeb88be7218b879d418c0ea0744702ae0
Prathamesh Ghorpade <ghorpadeprathamesh2411@gmail.com>
2023-03-15 14:17:20 +0530

    data

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -1,20 +1,20 @@
-const jwt =  require("jsonwebtoken")
-const JWT_SECRET = "prathamesh"
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'prathamesh';
 
-const fetchUser = (req,res,next) =>{
-    ///Get the\ user  from kwt token and addd id to req object
-    const token = req.header("auth-token")
-    if (!token ) {
-       res.status(401).send({error:'valide token is required'});
-    }
-    try {
-        
-        const data= jwt.verify(token,JWT_SECRET)
-        req.user = data.user
-           next() 
-    } catch (error) {
-        res.status(401).send({error:'valide token is required'});
-    }
-}
+const fetchUser = (req, res, next) => {
+  ///Get the\ user  from kwt token and addd id to req object
+  const token = req.header('auth-token');
+  if (!token) {
+    res.status(401).send({ error: 'valide token is required' });
+  }
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    console.log('data', data);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'valide token is required' });
+  }
+};
 
-module.exports = fetchUser
+module.exports = fetchUser;
